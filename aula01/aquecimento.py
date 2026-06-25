add98cadf4b55e1a727c380dad8965f1d72b6eaf
AdrianJR44 <adeyllsonadrian@gmail.com>
2026-06-24 21:35:33 -0300

    Update greetings to include 'Ádrian' in messages

--- aula01/aquecimento.py
+++ aula01/aquecimento.py
@@ -4,7 +4,7 @@ class Pessoa:
         self.email = email
 
     def dizer_oi(self) -> str:
-        return f"Oi, sou a/o {self.nome}"
+        return f"Oi, sou o Ádrian {self.nome}"
 
 class Aluno(Pessoa):
     def __init__(self, nome: str, email: str, matricula: str):
@@ -12,12 +12,12 @@ class Aluno(Pessoa):
         self.matricula = matricula
 
     def dizer_oi(self) -> str:
-        return f"Oi, sou a/o {self.nome} (matrícula {self.matricula})"
+        return f"Oi, sou o Ádrian {self.nome} (matrícula {self.matricula})"
 
 if __name__ == "__main__":
     pessoas: list[Pessoa] = [
-        Pessoa("João da Silva", "joao@exemplo.com"),
-        Aluno("Maria Souza", "maria@ufra.edu.br", "202601001")
+        Pessoa("Ádrian Vidinha", "Ádrian@exemplo.com"),
+        Aluno("Ádrian Gomes", "Ádrian@ufra.edu.br", "202601001")
     ]
 
     for pessoa in pessoas:
